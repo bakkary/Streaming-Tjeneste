92f5b56bf2293cc5ddaab314f1d43b4350ba8c18
XXOlsen <cph-xo@cphbusiness.dk>
2022-11-03 10:39:44 +0100

    DOUser

--- showandtell/src/User.java
+++ showandtell/src/User.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class User {
+
+    private int age;
+    private String username;
+    private String password;
+    private static int ID;
+
+    private ArrayList<watchedMovies> setefilm = new ArrayList<>;
+    private ArrayList<savedMovies> gemteFilm = new ArrayList<>;
+
+    public void login() {
+
+    }
+
+    public void watchedMovies() {
+
+    }
+
+    public void savedMovies() {
+
+    }
+
+
+    public void setwatchedMovies(ArrayList watchedMovies) {
+
+    }
+
+    public void setsavedMovies(ArrayList SavedMovies){
+
+    }
+
+    public User(int age, String username, String password){
+
+    }
 }
