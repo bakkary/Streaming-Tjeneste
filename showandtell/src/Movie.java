2b2e9a3613b74561aec18a3384a908783f5cf9c8
sygdrome <sygdrom@hotmail.com>
2022-11-03 11:05:55 +0100

    refactor

--- showandtell/src/Movie.java
+++ showandtell/src/Movie.java
@@ -1,31 +1,15 @@
 import java.util.ArrayList;
 
 public class Movie extends Content {
-    public static int ID;
     private int year;
 
-    Movie(String title, ArrayList category, float rating, int year) {
-        super(title, category, rating);
+    Movie(String title, ArrayList category, float rating, int year, int ID) {
+        super(title, category, rating, ID);
         this.year = year;
     }
 
     public int getYear() {
         return year;
-    }
-
-    @Override
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public ArrayList getCategories() {
-        return category;
-    }
-
-    @Override
-    public float getRating() {
-        return rating;
     }
 
     public void play() {
