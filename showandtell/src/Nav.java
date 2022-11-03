9992fd32acac6010f6d42b81c2bcdbf0cdaa55cc
DogusY03 <Dogusyildirim@icloud.com>
2022-11-03 11:17:22 +0100

    Merge

---
+++ showandtell/src/Nav.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Nav {
+
+    int age;
+
+    public String username;
+
+    public String password;
+
+    // public FileIO fileIO = new FileIO();
+    //private TextUI textUI = new TextUI();
+
+    public ArrayList<Serie> series = new ArrayList();
+
+    public ArrayList<Movie> movies = new ArrayList();
+
+    public User user = new User;
+
+
+
+    public void logout() {
+
+
+    }
+
+    private String signUp() {
+        return null;
+    }
+}
