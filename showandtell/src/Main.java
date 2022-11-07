95b6546020a77b3b527629055a9d42c9e0075bb5
Andreas Pihl <54024731+bakkary@users.noreply.github.com>
2022-11-07 09:54:36 +0100

    display options

--- showandtell/src/Main.java
+++ showandtell/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
+    private static Nav nav = new Nav();
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        nav.startMenu();
     }
 }
