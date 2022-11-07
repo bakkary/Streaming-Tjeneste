978447b67f72c6443778e994d68f561d34342c72
ChriZZZy <60019587+ChriZZZy@users.noreply.github.com>
2022-11-07 10:43:04 +0100

    Nav update / main split

--- showandtell/src/Nav.java
+++ showandtell/src/Nav.java
@@ -2,54 +2,37 @@ import java.util.ArrayList;
 
 public class Nav {
 
+    private User u;
     public FileIO fileIO = new FileIO();
     private TextUI textUI = new TextUI();
 
-    public ArrayList<Series> series = new ArrayList();
 
-    public ArrayList<Movie> movies = new ArrayList();
-
-    public User user;
-
-    public void startMenu(){
-        String result = textUI.getUserInput("Welcome! \n Press 1 to sign up \n Press 2 to log in");
-        if(Integer.parseInt(result) == 1){
-            signUp();
-        }else{
-            login();
-        }
-        System.out.println(user.getUsername());
-    }
-    public void login() {
-        String username = textUI.getUserInput("Please type your username: ");
-        String password = textUI.getUserInput("Please type your password: ");
-        String result = fileIO.readUserData(username, password);
-        String[] arr = result.split(";");
-        user = new User(arr[0],arr[1], Integer.parseInt(arr[2]));
+    public Nav(User u){
+        this.u = u;
     }
 
-    public void logout() {
-        String result = textUI.getUserInput("Are you sure you want to logout? \n Press 1 to logout \n Press 2 to go back");
-                if(Integer.parseInt(result) == 1){
-                    user = null;
-
-                    startMenu();
-                    //  } else{ mainMenu();
-
-
-        }
-
+    private void mainMenu(){
 
     }
 
-    private void signUp() {
-        String username = textUI.getUserInput("Please type your username: ");
-        String password = textUI.getUserInput("Please type your password: ");
-        int age = Integer.parseInt(textUI.getUserInput("Please type your age: "));
+    private void searchByTitle(){
 
-        user = new User(username, password, age);
-        fileIO.writeUserData(user);
     }
+    private void movieAction(){
+
+    }
+
+    private void viewSaved(){
+
+    }
+
+    private void viewWatched(){
+
+    }
+
+
+
+
 
 
 }
