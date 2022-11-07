4ff8f082da74971fdb89322578a111fa6b32fb1a
XXOlsen <cph-xo@cphbusiness.dk>
2022-11-07 12:25:40 +0100

    movieAction done

--- showandtell/src/Nav.java
+++ showandtell/src/Nav.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Nav {
 
@@ -7,32 +8,45 @@ public class Nav {
     private TextUI textUI = new TextUI();
 
 
-    public Nav(User u){
+    public Nav(User u) {
         this.u = u;
     }
 
-    public void mainMenu(){
+    public void mainMenu() {
 
     }
 
-    private void searchByTitle(){
-
-    }
-    private void movieAction(){
+    private void searchByTitle() {
 
     }
 
-    private void viewSaved(){
+    private void movieAction(Movie mov) {
+        ArrayList<String> options = new ArrayList<String>(Arrays.asList("Play movie", "Add movie til list", "Remove movie from list");
+        String input = textUI.getUserInput("" + mov.getTitle() + "Please select one of the following", options);
+        switch (Integer.parseInt(input)) {
+            case 1:
+                System.out.println(mov);
+                u.setWatchedMovies(mov.getID());
+                break;
+            case 2:
+                u.setSavedMovies(mov.getID());
+                break;
+            case 3:
+                u.removeSavedMovie(mov.getID());
+                break;
+            default:
+                System.out.println("Please try again");
+        }
+        fileIO.updateUserData(u);
+    }
+
+    private void viewSaved() {
 
     }
 
-    private void viewWatched(){
+    private void viewWatched() {
 
     }
-
-
-
-
 
 
 }
