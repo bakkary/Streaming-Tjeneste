95750acea93df48f27d0be3164a2333af8fc7bb3
XXOlsen <cph-xo@cphbusiness.dk>
2022-11-07 12:34:17 +0100

    movieAction

--- showandtell/src/Nav.java
+++ showandtell/src/Nav.java
@@ -13,15 +13,39 @@ public class Nav {
     }
 
     public void mainMenu() {
+        String input = "";
+        while (!input.equalsIgnoreCase("Q")) {
 
+
+            ArrayList<String> options = new ArrayList<String>(Arrays.asList("Search by title", "Search by category", "View saved content", "View watched content"));
+            input = textUI.getUserInput("Please select one of the following", options);
+            switch (Integer.parseInt(input)) {
+                case 1:
+                    searchByTitle();
+                    break;
+                case 2:
+                    //searchByCategory();
+                    break;
+                case 3:
+                    viewSaved();
+                    break;
+                case 4:
+                    viewWatched();
+                    break;
+                default:
+                    System.out.println("try again");
+            }
+        }
     }
 
     private void searchByTitle() {
-
+        String input = textUI.getUserInput("write the title of the movie you wish to watch");
+        Movie n = fileIO.readMovieData("Title", input);
+        movieAction(n);
     }
 
     private void movieAction(Movie mov) {
-        ArrayList<String> options = new ArrayList<String>(Arrays.asList("Play movie", "Add movie til list", "Remove movie from list");
+        ArrayList<String> options = new ArrayList<String>(Arrays.asList("Play movie", "Add movie til list", "Remove movie from list"));
         String input = textUI.getUserInput("" + mov.getTitle() + "Please select one of the following", options);
         switch (Integer.parseInt(input)) {
             case 1:
