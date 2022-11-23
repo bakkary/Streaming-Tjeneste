dde6a91c99d7c447fcaeee6a3b8d8eaa398e8aaa
Chris_ziegler <60019587+ChriZZZy@users.noreply.github.com>
2022-11-23 11:52:15 +0100

    update again

--- showandtell/src/Main.java
+++ showandtell/src/Main.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-     //StartMenu start = new StartMenu();
-       SQLIO sql = new SQLIO();
+        //StartMenu start = new StartMenu();
+        SQLIO sql = new SQLIO();
         sql.establishConnection();
-        sql.readUserData("chris");
+        sql.readUserData("chris", "haha");
     }
 }
--- showandtell/src/SQLIO.java
+++ showandtell/src/SQLIO.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 
 
 
-    public class SQLIO {
+
+    public class SQLIO implements Expresso {
 
         public SQLIO() {
             try
@@ -19,83 +20,10 @@ import java.util.ArrayList;
 
         private Connection connection;
 
-        private String url = "jdbc:mysql://localhost/world?" + "autoReconnect=true&useSSL=false";
+        private String url = "jdbc:mysql://localhost/streaming?" + "autoReconnect=true&useSSL=false";
         private String username = "root";
-        private String password = "7gaZXHlXt8y6^#0u";
+        private String password = "Pihkxl123123";
         private ArrayList<String> users = new ArrayList<>();
-
-        public void run()
-        {
-            // connection
-            establishConnection();
-
-            // statement
-            String query = "SELECT * FROM city ORDER BY population DESC LIMIT 10";
-
-            try
-            {
-                Statement statement = this.connection.createStatement();
-                statement.execute(query);
-
-                ResultSet resultSet = statement.getResultSet();
-
-                while(resultSet.next())
-                {
-                    String name = resultSet.getString("username");
-                    String password = resultSet.getString("password");
-
-
-                }
-            }
-            catch (SQLException e)
-            {
-                e.printStackTrace();
-            }
-
-
-          readUserData("chris");
-        }
-
-
-        public String readUserData(String username) {
-
-
-            String query = ("SELECT * FROM users WHERE username = '" +username+"'LIMIT 1");
-            try
-            {
-                Statement statement = this.connection.createStatement();
-                statement.execute(query);
-
-                ResultSet resultSet = statement.getResultSet();
-
-                while(resultSet.next())
-                {
-                    String name = resultSet.getString("username");
-                    String password = resultSet.getString("Password");
-
-                    users.add(username);
-
-                    System.out.println(users);
-
-
-                }
-            }
-            catch (SQLException e)
-            {
-                e.printStackTrace();
-            }
-            return "";
-
-        }
-
-/*
-        //private void writeUserData(int ID, String username, String password) {
-            String query = ("INSERT INTO users (ID,username,password) VALUES("+ID+",'"+username+"','"+password+"'");
-            Resultset result = new
-
-
-        }
-*/
 
 
 
@@ -111,6 +39,76 @@ import java.util.ArrayList;
             {
                 e.printStackTrace();
             }
+        }
+
+        @Override
+        public String[] readSeriesData(String field, String query) {
+
+            return new String[0];
+        }
+
+        @Override
+        public String[] readMovieData(String field, String query) {
+            return new String[0];
+        }
+
+        @Override
+        public String[] readUserData(String u, String p) {
+            String query = ("SELECT * FROM users WHERE username = '" +u+"''" +p+"'LIMIT 1");
+            try
+            {
+                Statement statement = this.connection.createStatement();
+                statement.execute(query);
+
+                ResultSet resultSet = statement.getResultSet();
+                ArrayList<String> users = new ArrayList<>();
+                Object[] usersarray;
+
+                    String ID = resultSet.getString("ID");
+                    String username = resultSet.getString("username");
+                    String password = resultSet.getString("password");
+                    System.out.println(1);
+
+                    users.add(ID);
+                    users.add(username);
+                    users.add(password);
+
+                    usersarray = users.toArray();
+
+
+
+
+            }
+            catch (SQLException e)
+            {
+                e.printStackTrace();
+            }
+
+            return new String[0];
+        }
+
+        @Override
+        public ArrayList searchCategories(int cat) {
+            return null;
+        }
+
+        @Override
+        public ArrayList<String> movieCat(String field, int userInput) {
+            return null;
+        }
+
+        @Override
+        public void writeUserData(User u) {
+            //String query = ("INSERT INTO users (ID,username,password) VALUES("+ID+",'"+username+"','"+password+"'");
+            //Resultset result = new
+
+
+        }
+
+
+        @Override
+        public void updateUserData(User u) {
+
         }
     }
 
